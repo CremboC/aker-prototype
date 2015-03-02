51da0d347b3fb02bb6a5a36630a98f9cdb26ca8a
CremboC <cremboc@gmail.com>
2015-03-02 13:26:41 +0000

    Changing product in work order now works as intended (samples are refiltered appropriately)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -68,170 +68,195 @@ $.fn.watcher = function () {
     return this;
 };
 
-/**
- * Asynchronously load data from a source
- *
- * @param options
- * @returns {$.fn}
- */
-$.fn.pager = function (options) {
-    var defaults = {
-        template: '#entry-template',
-        loadButton: '', // .jqp-load-more
-        appendBefore: '', // .jqp-append-before
-        metadata: '', // .jqp-metadata
-        preload: true,
-        scrollLoad: false,
-        loadEvents: 'click',
-        url: '',
-        dataType: 'json',
-        type: 'GET',
-        data: {}
-    };
+(function ($) {
 
-    var settings = $.extend({}, defaults, options);
+    /**
+     * Asynchronously load paged data from a source
+     *
+     * @param options
+     * @returns {$.fn}
+     */
+    $.fn.pager = function (options) {
 
-    // cachce all important variables and selectors
-    var $paginate = $(this),
-        $template = $(settings.template),
-        $loadButton = $(settings.loadButton),
-        $appendBefore = $(settings.appendBefore),
-        $metadata = $(settings.metadata);
+        var defaults = {
+            template: '#entry-template',
+            loadButton: '', // .jqp-load-more
+            appendBefore: '', // .jqp-append-before
+            metadata: '', // .jqp-metadata
+            preload: true,
+            scrollLoad: false,
+            loadEvents: 'click',
+            url: '',
+            dataType: 'json',
+            type: 'GET',
+            data: {}
+        };
 
-    if (settings.scrollLoad) {
-        settings.loadEvents += ' on.screen';
+        var settings;
 
-        $loadButton.watcher();
-    }
+        settings = $.extend({}, defaults, options);
 
-    // fetch original load button text so we can restore it after changing it to "Loading"
-    var originalLoadButtonText = $loadButton.html();
+        // cache all important variables and selectors
+        var $this = $(this),
+            $template = $(settings.template),
+            $loadButton = $(settings.loadButton),
+            $appendBefore = $(settings.appendBefore),
+            $metadata = $(settings.metadata);
 
-    // will be changed on first load
-    var totalPages = 0,
-        isLastPage = false;
+        var appendClass = "jqp-appended";
 
-    // set first page
-    $loadButton.data('page', 0);
-
-    // compile template using Handlebars
-    var source = $template.html();
-    var template = Handlebars.compile(source);
-
-    // increase the page number, makes sure one cannot load more pages than there are in total
-    var increasePage = function () {
-        var currentPage = parseInt($loadButton.data('page'));
-
-        if (currentPage + 1 > totalPages) {
-            $loadButton.hide();
-        } else {
-            $loadButton.data('page', currentPage + 1);
-        }
-    };
-
-    // update the loader button, changing between "loading" and normal state
-    var updateLoaderButton = function () {
-        var text = $loadButton.html();
-
-        if (originalLoadButtonText == text) {
-            $loadButton.html("<span class='glyphicon glyphicon-refresh glyphicon-spin'></span> Loading");
-            $loadButton.attr('disabled', 'disabled');
-        } else {
-            $loadButton.html(originalLoadButtonText);
-            $loadButton.removeAttr('disabled');
-        }
-    };
-
-    var updateMetadata = function (metadata) {
-        var current = (metadata.number + 1) * metadata.numberOfElements;
-
-        if (metadata.last) {
-            current = metadata.totalElements;
+        if (settings.scrollLoad) {
+            settings.loadEvents += ' on.screen';
+            $loadButton.watcher();
         }
 
-        $metadata.html("Showing " + current + " out of " + metadata.totalElements);
-    };
+        // fetch original load button text so we can restore it after changing it to "Loading"
+        var originalLoadButtonText = $loadButton.html();
 
-    // after loading the final page, cleanup
-    var cleanup = function () {
-        $(this).attr('disabled', 'disabled');
-        $loadButton.remove();
-        $loadButton.off('click on.screen');
-    };
+        // will be changed on first load
+        var totalPages = 0,
+            isLastPage = false;
 
-    // default method upon successful query
-    var success = settings.success ? settings.success :
-        function (data) {
-            console.log(data);
-            if (data.content.length > 0) {
-                // get total pages from Spring's Pageable
-                totalPages = data.totalPages;
-                // get the last page
-                isLastPage = data.last;
+        // set first page
+        $loadButton.data('page', 0);
 
-                increasePage();
+        // compile template using Handlebars
+        var source = $template.html();
+        var template = Handlebars.compile(source);
 
-                // convert the json content into html using the template
-                var sampleHtml = $.map($.makeArray(data.content), function (sample) {
-                    return template(sample);
-                });
+        // increase the page number, makes sure one cannot load more pages than there are in total
+        var increasePage = function () {
+            var currentPage = parseInt($loadButton.data('page'));
 
-                $appendBefore.before(sampleHtml);
-                updateLoaderButton();
-                updateMetadata(data);
-
-                var currentPage = $loadButton.data('page');
-                if (currentPage >= totalPages) {
-                    cleanup();
-                }
+            if (currentPage + 1 > totalPages) {
+                $loadButton.hide();
             } else {
-                cleanup();
+                $loadButton.data('page', currentPage + 1);
             }
         };
 
-    // default method upon error
-    var error = settings.error ? settings.error :
-        function (xhr, status, errorThrown) {
-            console.log(xhr);
-            console.log(status);
-            console.log(errorThrown);
+        // update the loader button, changing between "loading" and normal state
+        var updateLoaderButton = function () {
+            var text = $loadButton.html();
+
+            if (originalLoadButtonText == text) {
+                $loadButton.html("<span class='glyphicon glyphicon-refresh glyphicon-spin'></span> Loading");
+                $loadButton.attr('disabled', 'disabled');
+            } else {
+                $loadButton.html(originalLoadButtonText);
+                $loadButton.removeAttr('disabled');
+            }
         };
 
-    // wrapper to $.ajax
-    var load = function () {
+        var updateMetadata = function (metadata) {
+            var current = (metadata.number + 1) * metadata.numberOfElements;
 
-        var data = $.merge(settings.data, {
-            page: $loadButton.data('page'),
-            size: 20
-        });
+            if (metadata.last) {
+                current = metadata.totalElements;
+            }
 
-        $.ajax(settings.url, {
-            dataType: settings.dataType,
-            type: settings.type,
-            data: data,
-            success: success,
-            error: error
-        }).then(function () {
-            $paginate.trigger('pager.loaded')
-        });
-    };
+            $metadata.html("Showing " + current + " out of " + metadata.totalElements);
+        };
 
-    if (!isLastPage) {
+        // after loading the final page, finish
+        var finish = function () {
+            $loadButton.attr('disabled', 'disabled');
+            $loadButton.hide();
+            $loadButton.data('page', 0);
+        };
+
+        // default method upon successful query
+        var success = settings.success ? settings.success :
+            function (data) {
+                if (data.content.length > 0) {
+                    // get total pages from Spring's Pageable
+                    totalPages = data.totalPages;
+                    // get the last page
+                    isLastPage = data.last;
+
+                    increasePage();
+
+                    // convert the json content into html using the template
+                    var sampleHtml = $.map($.makeArray(data.content), function (sample) {
+                        return $(template(sample)).addClass(appendClass);
+                    });
+
+                    $appendBefore.before(sampleHtml);
+                    updateLoaderButton();
+                    updateMetadata(data);
+
+                    var currentPage = $loadButton.data('page');
+                    if (currentPage >= totalPages) {
+                        finish();
+                    }
+                } else {
+                    finish();
+                }
+            };
+
+        // default method upon error
+        var error = settings.error ? settings.error :
+            function (xhr, status, errorThrown) {
+                console.log(xhr);
+                console.log(status);
+                console.log(errorThrown);
+            };
+
+        // wrapper to $.ajax
+        var load = function () {
+            var data = $.extend(settings.data, {
+                page: $loadButton.data('page'),
+                size: 20
+            });
+
+            $.ajax(settings.url, {
+                dataType: settings.dataType,
+                type: settings.type,
+                data: data,
+                success: success,
+                error: error
+            }).then(function () {
+                $this.trigger('pager.loaded')
+            });
+        };
+
         $loadButton.on(settings.loadEvents, function (e) {
             e.preventDefault();
+            if (!isLastPage) {
+                updateLoaderButton();
+                load();
+            }
+        });
+
+        // immediately load first page
+        if (settings.preload) {
             updateLoaderButton();
             load();
-        });
-    }
+        }
 
-    // immediately load first page
-    if (settings.preload) {
-        updateLoaderButton();
-        load();
-    }
+        return {
+            load: function (options) {
+                var localDefaults = {
+                    cleanup: true,
+                    data: {}
+                };
 
-    return this;
-};
+                var localSettings = $.extend({}, localDefaults, options);
+
+                settings.data = localSettings.data;
+
+                if (localSettings.cleanup) {
+                    $metadata.html('');
+                    $('.' + appendClass).remove();
+                }
+
+                load();
+            },
+            chain: this
+        };
+    };
+
+}(jQuery));
 
 /**
  * Makes and element which has a checkbox inside to be checkable by clicking anywhere on the element itself.
